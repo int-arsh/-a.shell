aea7752d1ccca58679c0a34ec7c386225d8aba10
int-arsh <jainaakash303@gmail.com>
2025-10-06 01:04:41 +0530

    Add new project 'Echo' to ProjectsList with description and links

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,6 +1,12 @@
 import '../App.css';
 
 const projects = [
+    {
+        title: 'Echo',
+        description: 'Social App built in Next.js 15, React, Tailwind CSS, MySQL featuring user authentication, real-time chat, and post sharing functionalities.',
+        github_link: 'https://github.com/int-arsh/Echo',
+        live_link: 'https://echo-beta-six.vercel.app/'
+    },
     
     {
         title: 'Chattr',
